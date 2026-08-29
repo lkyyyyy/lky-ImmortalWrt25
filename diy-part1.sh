ea8e25d49282baa4b711a9d3c52456f1a904b982
lkyyyyy <86304293+lkyyyyy@users.noreply.github.com>
2026-08-29 11:56:11 +0800

    Switch to official immortalwrt openwrt-24.10: update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,6 +9,8 @@
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
+# 适用源码: immortalwrt/immortalwrt 分支 openwrt-24.10
+# 说明: 官方 24.10 本身无 passwall，无需删 feed；仅追加 3 个第三方 feed。
 
 # Add qmodem feed (5G 模组管理)
 echo 'src-git qmodem https://github.com/BootLoopLover/qmodem.git' >> feeds.conf.default
